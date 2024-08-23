f42cd2e2cbbeb9e7f2577b8ccd38739b11421214
Adair Daniels <adairdaniels@gmail.com>
2024-08-22 22:13:31 -0700

    Initial setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# LanguageContext Tutorial
+
+### What is this?
+
+An example of a Context Provider for language switching.
+
+### Where would you use this?
+
+React applications with multilingual support.
+
+### Why Context?
+
+Context providers are lightweight and powerful, avoiding prop drilling on a state that often needs to get used in bottom level components.
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { LanguageContext } from './components/LanguageContext';
+import LanguageChange from './components/LanguageChange';
+import { translations } from './data/translations';
+
+function App() {
+  const { language } = useContext(LanguageContext);
+
+  return (
+    <div className="App">
+      <header>
+            <h1>{translations.appTitle[language]}</h1>
+      </header>
+      <main>
+        <p>{translations.welcomeMessage[language]}</p>
+        <LanguageChange />
+      </main>
+    </div>
+  );
+}
+
+export default App;
